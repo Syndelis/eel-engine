738ad2b54c22c0b466886dc5c93b95f227c522e2
Brenno Lemos <brenno@syndel.is>
2020-03-29 19:24:26 -0300

    Added a new example

---
+++ examples/gear.py
@@ -0,0 +1,61 @@
+from math import sin, cos, pi
+from time import time
+from sys import path
+
+ATTEMPTS = 4
+for i in range(ATTEMPTS):
+
+    try:
+        from eel import Eel
+        from figure import BaseFigure, Circle
+        break
+
+    except ModuleNotFoundError:
+        path.insert(0, '..')
+
+
+class Gear(BaseFigure):
+
+    def __init__(self, x, y, *, inner_radius, outer_radius,
+                dents=6, angle=0,precision=1):
+
+        self.x, self.y = x, y
+        self.inner_radius = inner_radius
+        self.outer_radius = outer_radius
+        self.dents = dents
+        self.angle = angle * pi / 180
+
+        self.used = round((inner_radius + outer_radius)/2 * 20 * precision)
+        self.dent_ratio = self.used // self.dents
+
+    
+    def layout(self):
+
+        l = []
+        for i in range(self.used):
+
+            a = pi * 2 / self.used * i + self.angle
+            if (i % self.dent_ratio < self.dent_ratio / 2):
+                r = self.inner_radius
+
+            else: r = self.outer_radius
+
+            l.append((cos(a)*r, sin(a)*r))
+
+        return l
+
+
+e = Eel()
+
+global g
+g = Gear.new(300, 200, inner_radius=40, outer_radius=55)
+
+@e.draw
+def render(eel):
+    global g
+
+    g.angle += 0.01
+    g(eel)
+    Circle.new(300, 200, radius=20)(eel)
+
+e.run()
